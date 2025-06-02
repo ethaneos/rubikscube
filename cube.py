9845f3b3568400615987ca5666925964b19230e4
Ethan C <eymchong@gmail.com>
2025-06-02 14:49:49 +1000

    Added creation functions

--- cube.py
+++ cube.py
@@ -13,16 +13,6 @@ colour = {
     "purple": vec(0.4,0.2,0.6),
     "black": vec(0,0,0)
 }
-def createPart(location, colours):
-    dispCalc = lambda lst, size: (lst+lst*0.02*size)
-    displacement = list(map(dispCalc, location, [sizeOfPart]*len(location)))
-    side5 = box(pos=vec(displacement[0], displacement[1], displacement[2]+sizeOfPart/2), length=sizeOfPart, height=sizeOfPart, width=sizeOfPart/50, color=colour[colours["f"]])
-    side6 = box(pos=vec(displacement[0], displacement[1], displacement[2]-sizeOfPart/2), length=sizeOfPart, height=sizeOfPart, width=sizeOfPart/50, color=colour[colours["o"]])
-    side1 = box(pos=vec(displacement[0]+sizeOfPart/2, displacement[1], displacement[2]), length=sizeOfPart/50, height=sizeOfPart, width=sizeOfPart, color=colour[colours["r"]])
-    side2 = box(pos=vec(displacement[0]-sizeOfPart/2, displacement[1], displacement[2]), length=sizeOfPart/50, height=sizeOfPart, width=sizeOfPart, color=colour[colours["l"]])
-    side3 = box(pos=vec(displacement[0], displacement[1]+sizeOfPart/2, displacement[2]), length=sizeOfPart, height=sizeOfPart/50, width=sizeOfPart, color=colour[colours["t"]])
-    side4 = box(pos=vec(displacement[0], displacement[1]-sizeOfPart/2, displacement[2]), length=sizeOfPart, height=sizeOfPart/50, width=sizeOfPart, color=colour[colours["b"]])
-    return [side1, side2, side3, side4, side5, side6]
 
 class Part:
     def __init__(self, location: list, colours: str):
@@ -34,55 +24,21 @@ class Part:
         pass
 
 
-right = compound(createPart(
-    [1,0,0],
-    {
-    "f": "black",
-    "o": "black",
-    "r": "orange",
-    "l": "black",
-    "t": "black",
-    "b": "black"
-}))
-left = compound(createPart(
-    [-1,0,0],
-    {
-    "f": "white",
-    "o": "red",
-    "r": "blue",
-    "l": "green",
-    "t": "cyan",
-    "b": "magenta"
-}))
-topleftfront = compound(createPart(
-    [-1,1,1],
-    {
-    "f": "white",
-    "o": "red",
-    "r": "blue",
-    "l": "green",
-    "t": "cyan",
-    "b": "magenta"
-}))
-frontright = compound(createPart(
-    [1,0,1],
-    {
-    "f": "white",
-    "o": "red",
-    "r": "blue",
-    "l": "green",
-    "t": "cyan",
-    "b": "magenta"
-}))
-
-def createSide(partLoc, sideLoc, colour):
-    side = box(pos=vec(partLoc[0]+sizeOfPart*sideLoc[1]/2, partLoc[1]+sizeOfPart*sideLoc[1]/2, partLoc[2]+sizeOfPart*sideLoc[2]/2), 
-               length=sizeOfPart/(50*abs(sideLoc[0])), height=sizeOfPart/(50*abs(sideLoc[1])), width=sizeOfPart/(50*abs(sideLoc[2])), 
-               color=colour[colour["f"]])
+def createSide(partLoc, sideLoc, colour_of_side):
+    side = box(pos=vec(partLoc[0]*1.1+sizeOfPart*sideLoc[0]/2, partLoc[1]*1.1+sizeOfPart*sideLoc[1]/2, partLoc[2]*1.1+sizeOfPart*sideLoc[2]/2), 
+               length=sizeOfPart-sizeOfPart*49/50*abs(sideLoc[0]), height=sizeOfPart-sizeOfPart*49/50*abs(sideLoc[1]), width=sizeOfPart-sizeOfPart*49/50*abs(sideLoc[2]), 
+               color=colour[colour_of_side])
 
 def initialisePart(location):
-    for i in range(len(location)):
-        pass
+    for axis in range(3):
+        for side in [-1,1]:
+            sideLoc = [0,0,0]
+            sideLoc[axis] = side
+            try:
+                colour_of_side = cube[tuple(sideLoc)][location]
+            except KeyError:
+                colour_of_side = "black"
+            createSide(location, sideLoc, colour_of_side)
 
 if __name__ == "__main__":
     cubeTop = {
@@ -121,5 +77,14 @@ if __name__ == "__main__":
                             (0,-1,0): cubeBottom
     }
 
-
+    new_scene = canvas(background = color.white)
+    for x in range(-1,2):
+        for y in range(-1,2):
+            for z in range(-1,2):
+                location = (x,y,z)
+                if location == (0,0,0):
+                    continue
+                else:
+                    initialisePart(location)
+    
     input("enter something to close")
